93b82088151c9a59360450eef3e71383e4ccc462
deaDiajeng <168566666+deaDiajeng@users.noreply.github.com>
2025-03-28 22:44:22 +0700

    add adminav.php & sidebar.php

---
+++ application/views/layout/sidebar.php
@@ -0,0 +1,40 @@
+        <!-- Sidebar -->
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="dashboard">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fas fa-laugh-wink"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3">Admin RQ</div>
+            </a>
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                CMS
+            </div>
+            <!-- CMS Links -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="Agenda">
+                    <i class="fas fa-fw fa-cog"></i>
+                    <span>Acara</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="Capaian">
+                        <i class="fas fa-fw fa-wrench"></i>
+                        <span>Capaian</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="Gallery"
+                    aria-expanded="true" aria-controls="collapseGaleri">
+                    <i class="fas fa-fw fa-cog"></i>
+                    <span>Galeri</span>
+                </a>
+            </li>
+        </ul>
+        <!-- End of Sidebar -->
